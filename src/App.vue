714ffcda2e9ee489643e5f1f28c841dbc2866032
André Pereira <andrepereirabe@outlook.com>
2025-07-04 22:39:54 +0200

    feat(core): Ignore build folder.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 			<img alt="Vite logo" class="logo" src="/vite.svg" />
 		</a>
 		<a href="https://vuejs.org/" target="_blank">
-			<img alt="Vue logo" class="logo vue" src="../assets/vue.svg" />
+			<img alt="Vue logo" class="logo vue" src="./assets/vue.svg" />
 		</a>
 	</div>
 	<HelloWorld msg="Vite + Vue" />
